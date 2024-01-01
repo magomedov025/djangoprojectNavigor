3f74b726b4ba4ad0654cfee2e8ad4d4b86ce51ed
ajax <ajax@MacBook-Pro-ajax.local>
2024-01-02 00:50:22 +0300

    update last commit

--- navigator/settings.py
+++ navigator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,11 +82,16 @@ WSGI_APPLICATION = 'navigator.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+         'default': {
+             'ENGINE': 'django.db.backends.postgresql',
+             'NAME': os.getenv('POSTGRES_DB', 'navigator_bd'),
+             'USER': os.getenv('POSTGRES_USER','ajax'),
+             'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+             'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+             'PORT': os.getenv('POSTGRES_PORT', 5432),
+         }
+     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -128,6 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
